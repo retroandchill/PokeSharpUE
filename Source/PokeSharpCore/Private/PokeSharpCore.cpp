1cace4dfb133bae84612e00fe8c66b159175c323
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-11-22 10:35:03 -0500

    Initial commit

---
+++ Source/PokeSharpCore/Private/PokeSharpCore.cpp
@@ -0,0 +1,17 @@
+﻿#include "PokeSharpCore.h"
+
+#define LOCTEXT_NAMESPACE "FPokeSharpCoreModule"
+
+void FPokeSharpCoreModule::StartupModule()
+{
+    
+}
+
+void FPokeSharpCoreModule::ShutdownModule()
+{
+    
+}
+
+#undef LOCTEXT_NAMESPACE
+    
+IMPLEMENT_MODULE(FPokeSharpCoreModule, PokeSharpCore)
