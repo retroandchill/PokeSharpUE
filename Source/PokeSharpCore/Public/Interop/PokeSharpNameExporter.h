9e256f49158b8d3688fae7c65ec80b505f4411d1
Retro & Chill <35567462+retroandchill@users.noreply.github.com>
2025-11-22 10:59:10 -0500

    Generated the C# project

---
+++ Source/PokeSharpCore/Public/Interop/PokeSharpNameExporter.h
@@ -0,0 +1,18 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "PokeSharpNameExporter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POKESHARPCORE_API UPokeSharpNameExporter : public UObject
+{
+	GENERATED_BODY()
+	
+	
+};
